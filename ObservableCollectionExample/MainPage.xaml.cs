be1b05974c017c3717c66f48161abbe37aeae077
PhamBaoNgoc <59905079+PhamBaoNgoc@users.noreply.github.com>
2020-08-18 21:45:59 +0700

    Add project files.

---
+++ ObservableCollectionExample/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ObservableCollectionExample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private List<Icon> Icons;
+        private ObservableCollection<Contact> Contacts;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Icons = new List<Icon>();
+            Icons.Add(new Icon { IconPath = "Imager/1.png" });
+            Icons.Add(new Icon { IconPath = "Imager/2.png" });
+            Icons.Add(new Icon { IconPath = "Imager/3.png" });
+            Icons.Add(new Icon { IconPath = "Imager/4.png" });
+            Icons.Add(new Icon { IconPath = "Imager/5.png" });
+
+            Contacts = new ObservableCollection<Contact>();
+        }
+        private void NewContactButton_Click(object sender, RoutedEventArgs e)
+        {
+            string avatar = ((Icon)AvatarComboBox.SelectedValue).IconPath;
+            Contacts.Add(new Contact
+            {
+                FirthName = FirthNameTextBox.Text,
+                LastName = LastNameTextBox.Text,
+                AvatarPath = avatar
+            });
+            if (LastNameTextBox != null && FirthNameTextBox != null)
+            {
+
+            }
+            else
+            {
+                FirthNameTextBox.Text = "May chua ghi ten";
+                LastNameTextBox.Text = "Vao day";
+            }
+
+            AvatarComboBox.SelectedIndex = -1;
+            FirthNameTextBox.Focus(FocusState.Programmatic);
+        }
+    }
+}
---
+++ ObservableCollectionExample/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableCollectionExample.Models
+{
+    public class Contact
+    {
+        public string FirthName { get; set; }
+        public string LastName { get; set; }
+        public string AvatarPath { get; set; }
+    }
+}
